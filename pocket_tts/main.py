01a4e72fc035ce55bb54f92eb815b9c1103562ed
Djima <saleemdjimabach@gmail.com>
2026-01-17 17:31:29 +0100

    feat(cli): add max_tokens parameter to limit tokens in generate command

--- pocket_tts/main.py
+++ pocket_tts/main.py
@@ -22,6 +22,7 @@ from pocket_tts.default_parameters import (
     DEFAULT_NOISE_CLAMP,
     DEFAULT_TEMPERATURE,
     DEFAULT_VARIANT,
+    MAX_TOKEN_PER_CHUNK,
 )
 from pocket_tts.models.tts_model import TTSModel
 from pocket_tts.utils.logging_utils import enable_logging
@@ -87,7 +88,7 @@ def write_to_queue(queue, text_to_generate, model_state):
             self.queue.put(None)
 
     audio_chunks = tts_model.generate_audio_stream(
-        model_state=model_state, text_to_generate=text_to_generate
+        model_state=model_state, text_to_generate=text_to_generate,
     )
     stream_audio_chunks(FileLikeToQueue(queue), audio_chunks, tts_model.config.mimi.sample_rate)
 
@@ -222,6 +223,9 @@ def generate(
         str, typer.Option(help="Output path for generated audio")
     ] = "./tts_output.wav",
     device: Annotated[str, typer.Option(help="Device to use")] = "cpu",
+    max_tokens: Annotated[
+        int, typer.Option(help="Maximum number of tokens per chunk.")
+    ] = MAX_TOKEN_PER_CHUNK,
 ):
     """Generate speech using Kyutai Pocket TTS."""
     if "cuda" in device:
@@ -241,6 +245,7 @@ def generate(
             model_state=model_state_for_voice,
             text_to_generate=text,
             frames_after_eos=frames_after_eos,
+            max_tokens=max_tokens
         )
 
         stream_audio_chunks(output_path, audio_chunks, tts_model.config.mimi.sample_rate)

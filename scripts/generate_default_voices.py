24cb49e8152619f83c89d3478c234138f6f857d6
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2026-04-08 14:46:41 +0200

    Add german

--- scripts/generate_default_voices.py
+++ scripts/generate_default_voices.py
@@ -19,7 +19,7 @@ for voice_name, voice_origin in _ORIGINS_OF_PREDEFINED_VOICES.items():
 
     audio = model.generate_audio(
         model_state_copy,
-        "O sol nasceu lentamente atrás das montanhas, pintando o céu de tons dourados e rosados.",
+        "An Sommernachmittagen spielten die Kinder fröhlich auf dem Platz der kleinen Stadt.",
     )
     scipy.io.wavfile.write(
         f"./built-in-voices-generated/{voice_name}.wav", model.sample_rate, audio.numpy()

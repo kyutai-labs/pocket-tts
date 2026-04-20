c63c393210a8fae3931be2b18656de692fd73d90
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2026-04-20 15:44:33 +0200

    saved

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ set -ex
 export COMMIT_SHA=$(git rev-parse --short HEAD)
 docker buildx bake -f docker-bake.hcl --push
 
-docker -H ssh://root@51.159.138.238 stack deploy \
+docker -H ssh://root@pocket-tts-api.kyutai.org stack deploy \
     --with-registry-auth -c swarm-config.yaml pocket-tts
